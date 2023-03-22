a82389484e758671d06bc898ddd1d678133744d0
manikantatholeti <manikantathooleti9@gmail.com>
2023-03-22 16:21:59 +0530

    addded module

--- ec2/ec2_terraform.tf
+++ ec2/ec2_terraform.tf
@@ -1,15 +1,15 @@
-provider "aws" {
-  profile    = "default"
-  region     = "ap-south-1"
-}
+# provider "aws" {
+#   profile    = "default"
+#   region     = "ap-south-1"
+# }
 
-resource "aws_instance" "sample" {
-  ami           = "ami-0f8ca728008ff5af4"
-  instance_type = "t2.micro"
-  key_name  = "veeru"
-  # vpc_security_group_ids = "sg-033636df704a2c90f"
-  security_groups = ["launch-wizard-1","default"]
-    tags = {
-    "Name" = "jenkinstf"
-  }
-}
+# resource "aws_instance" "sample" {
+#   ami           = "ami-0f8ca728008ff5af4"
+#   instance_type = "t2.micro"
+#   key_name  = "veeru"
+#   # vpc_security_group_ids = "sg-033636df704a2c90f"
+#   security_groups = ["launch-wizard-1","default"]
+#     tags = {
+#     "Name" = "jenkinstf"
+#   }
+# }
---
+++ modules/ec2-module/resources.tf
@@ -0,0 +1,12 @@
+resource "aws_instance""test-instance" {
+  ami           = "ami-0f8ca728008ff5af4"
+  instance_type = "t2.micro"
+  key_name  = "vkkey"
+  subnet_id = "subnet-0863fe1077f34387d"
+  associate_public_ip_address = true
+  # vpc_security_group_ids = "sg-033636df704a2c90f"
+  security_groups = ["sg-033636df704a2c90f"]
+    tags = {
+    "Name" = "test-instance"
+  }
+}
