50edc7dcbe25db117ef18567ba1b000e4f6f4528
manikantatholeti <manikantathooleti9@gmail.com>
2023-03-23 13:52:57 +0530

    ec2 modules created

--- ec2/ec2_terraform.tf
+++ ec2/ec2_terraform.tf
@@ -17,5 +17,11 @@
 module "ec2_instance" {
   source  = "../modules/ec2-module"
   instancetype = "t2.micro"
+  ami = "ami-0f8ca728008ff5af4"
+  key_name = "vkkey"
+  subnet_id = "subnet-0863fe1077f34387d"
+  associate_public_ip_address = true
+  # security_groups = "sg-033636df704a2c90f"
+  Name = "test-instance"
   # version = "~> 4.0"
 }
--- modules/ec2-module/resources.tf
+++ modules/ec2-module/resources.tf
@@ -1,13 +1,13 @@
 resource "aws_instance""test-instance" {
-  ami           = "ami-0f8ca728008ff5af4"
+  ami           = var.ami
   instance_type = var.instancetype
 #   instance_type = "t2.micro"
-  key_name  = "vkkey"
-  subnet_id = "subnet-0863fe1077f34387d"
-  associate_public_ip_address = true
+  key_name  = var.key_name
+  subnet_id = var.subnet_id
+  associate_public_ip_address = var.associate_public_ip_address
   # vpc_security_group_ids = "sg-033636df704a2c90f"
   security_groups = ["sg-033636df704a2c90f"]
     tags = {
-    "Name" = "test-instance"
+    "Name" = var.Name
   }
 }
